3ac9fdd8d5419fd6a4c836464014a2fd1124026d
Robbie Richards <robbierichards@kubrickgroup.com>
2023-03-09 16:22:04 +0000

    3c added. all of 3 included

--- ref_data.py
+++ ref_data.py
@@ -5,6 +5,7 @@ import yahoofinancials
 from yahoofinancials import YahooFinancials
 import pandas as pd
 import numpy as np
+import os
 
 
 def get_sp100():
@@ -61,3 +62,54 @@ def get_yahoo_data(start_date: str,end_date: str,tickers: list=get_sp100(),time_
     df=df[['date','high','low','price','volume','1daily_return','2daily_return','3daily_return','5daily_return','10daily_return','symbol']]
 
     return df
+
+
+
+def get_sentiment_word_dict():
+    # Define the path of the file containing the sentiment word data
+    # os.chdir(r"C:\\Users\\Nixon Ng\\OneDrive - Kubrick Group\\Desktop\\Kubrick Training Program\\Week 10 (Edgar)\\edgar_project\\edgar-bps")
+    # filename = rf'../edgar-bps/lm_dict_1993-2021.csv'    
+    cwd = os.getcwd()
+    os.chdir(cwd)
+    filename = os.fsdecode('lm_dict_1993-2021.csv')
+    # Create the dictionary containing sentiments as dictionary keys, and list of sentiment words as dictionary values    
+    sentiment_dict = {'Positive': [], 'Negative': [], 'Uncertainty': [], 'Litigious': [], 
+                      'Strong Modal': [], 'Weak Modal': [], 'Constraining': [], 'Superfluous': [], 
+                      'Interesting': [], 'Modal': []}
+    # Read the csv file using pandas
+    lm_df = pd.read_csv(filename)
+    # Optional: Remove rows for non-essential columns    
+    # lm_df = lm_df.drop(columns = ['Seq_num','Word Count','Word Proportion','Average Proportion','Std Dev','Doc Count','Syllables','Source'])
+    # Create the filters for each dictionary key    
+    mask1 = lm_df['Positive'] > 0 
+    mask2 = lm_df['Negative'] > 0
+    mask3 = lm_df['Uncertainty'] > 0
+    mask4 = lm_df['Litigious'] > 0
+    mask5 = lm_df['Strong_Modal'] > 0
+    mask6 = lm_df['Weak_Modal'] > 0
+    mask7 = lm_df['Constraining'] > 0
+    # Integrate the filters to create new dataframes    
+    sentiment_word_df1 = lm_df[mask1]
+    sentiment_word_df2 = lm_df[mask2]
+    sentiment_word_df3 = lm_df[mask3] 
+    sentiment_word_df4 = lm_df[mask4] 
+    sentiment_word_df5 = lm_df[mask5]
+    sentiment_word_df6 = lm_df[mask6]
+    sentiment_word_df7 = lm_df[mask7]
+    # Using new dataframes, for loop is used to append the words into the sentiment dictionary
+    for row_dict in sentiment_word_df1.to_dict(orient='records'):
+        sentiment_dict['Positive'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df2.to_dict(orient='records'):
+        sentiment_dict['Negative'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df3.to_dict(orient='records'):
+        sentiment_dict['Uncertainty'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df4.to_dict(orient='records'):
+        sentiment_dict['Litigious'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df5.to_dict(orient='records'):
+        sentiment_dict['Strong Modal'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df6.to_dict(orient='records'):
+        sentiment_dict['Weak Modal'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df7.to_dict(orient='records'):
+        sentiment_dict['Constraining'].append(row_dict['Word'])
+    # Return the dictionary of sentiment words    
+    return sentiment_dict
