ea258b3dbb54850d53cbf0b44387b15842071baf
Arun Jandor <arunjandor@kubrickgroup.com>
2023-03-09 09:38:35 +0000

    brokencode

---
+++ testing.py
@@ -0,0 +1,11 @@
+import edgar_downloader
+import jonscode
+import kylescode
+
+edgar_downloader.download_files_10k('NEE', 'C:\\NotOneDrive\\Edgar\\htmls')
+
+jonscode
+
+kylescode  
+
+return endfile
