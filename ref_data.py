90eab434cfd35359f57c6bf3e15d128bf474ae27
Robbie Richards <robbierichards@kubrickgroup.com>
2023-03-10 09:25:45 +0000

    3c added superfluous and sorted modal

--- ref_data.py
+++ ref_data.py
@@ -78,6 +78,27 @@ def get_sentiment_word_dict():
                       'Interesting': [], 'Modal': []}
     # Read the csv file using pandas
     lm_df = pd.read_csv(filename)
+    lm_df['Modal'] = lm_df.apply(lambda row: row['Strong_Modal'] + row['Weak_Modal'], axis = 1)
+    superfluous_df = pd.DataFrame([['EXCESSIVE',1], ['EXTREME',1], ['REMAINING',1],['EXPENDABLE',1],['RESIDUARY',1],
+                                   ['GRATUITOUS',1],['INESSENTIAL',1],['SPARE',1],['REDUNDANT',1],['INORDINATE',1],
+                                   ['SUPERABUNDANT',1],['UNNEEDED',1],['LAVISH',1],['SUPEREROGATORY',1],['USELESS',1],
+                                   ['LEFTOVER',1],['SUPERFLUENT',1],['ABOUNDING',1],['NEEDLESS',1],['SUPERNUMERARY',1],
+                                   ['NONESSENTIAL',1],['SURPLUS',1],['DISPENSABLE',1],['OVERFLOWING',1],['UNASKED',1],
+                                   ['EXCESS',1],['OVERMUCH',1],['UNCALLED-FOR',1],['EXORBITANT',1],['PLEONASTIC',1],
+                                   ['UNREQUIRED',1],['EXTRAVAGANT',1],['PROFUSE',1],['UNWATED',1]], 
+                                   columns = ['Word','Superfluous'])
+    interesting_df = pd.DataFrame([['ALLURING',1],['READABLE',1],['GRIPPING',1],['AMUSING',1],['REFRESHING',1],
+                                   ['INVITING',1],['ATTRACTIVE',1],['STIMULATING',1],['PREPOSSESSING',1],['BEAUTIFUL',1],
+                                   ['STRIKING',1],['RIVETING',1],['COMPELLING',1],['THOUGHT-PROVOKING',1],['STIRRING',1],
+                                   ['CURIOUS',1],['UNUSUAL',1],['WINNING',1],['DELIGHTFUL',1],['ABSORBING',1],['CHARISMATIC',1],
+                                   ['ENGAGING',1],['AFFECTING',1],['ELEGANT',1],['EXOTIC',1],['ARRESTING',1],['EXCEPTIONAL',1],
+                                   ['FASCINATING',1],['CAPTIVATING',1],['GRACIOUS',1],['IMPRESSIVE',1],['ENCHANTING',1],
+                                   ['MAGNETIC',1],['INTRIGUING',1],['ENGROSSING',1],['PLEASURABLE',1],['LOVELY',1],
+                                   ['ENTHRALLING',1],['SUSPICIOUS',1],['PLEASING',1],['ENTRANCING',1],['SUSPICIOUS',1],
+                                   ['PLEASING',1],['ENTRANCING',1],['PROVOCATIVE',1],['FINE',1]], columns = ['Word','Interesting'])
+    lm_df = pd.concat([lm_df, superfluous_df], axis = 0)
+    lm_df = pd.concat([lm_df, interesting_df], axis = 0)
+    lm_df = lm_df.fillna(0)
     # Optional: Remove rows for non-essential columns    
     # lm_df = lm_df.drop(columns = ['Seq_num','Word Count','Word Proportion','Average Proportion','Std Dev','Doc Count','Syllables','Source'])
     # Create the filters for each dictionary key    
@@ -88,6 +109,10 @@ def get_sentiment_word_dict():
     mask5 = lm_df['Strong_Modal'] > 0
     mask6 = lm_df['Weak_Modal'] > 0
     mask7 = lm_df['Constraining'] > 0
+    mask8 = lm_df['Superfluous'] > 0
+    mask9 = lm_df['Interesting'] > 0
+    mask10 = lm_df['Modal'] > 0
+
     # Integrate the filters to create new dataframes    
     sentiment_word_df1 = lm_df[mask1]
     sentiment_word_df2 = lm_df[mask2]
@@ -96,6 +121,10 @@ def get_sentiment_word_dict():
     sentiment_word_df5 = lm_df[mask5]
     sentiment_word_df6 = lm_df[mask6]
     sentiment_word_df7 = lm_df[mask7]
+    sentiment_word_df8 = lm_df[mask8]
+    sentiment_word_df9 = lm_df[mask9]
+    sentiment_word_df10 = lm_df[mask10]
+
     # Using new dataframes, for loop is used to append the words into the sentiment dictionary
     for row_dict in sentiment_word_df1.to_dict(orient='records'):
         sentiment_dict['Positive'].append(row_dict['Word'])
@@ -111,5 +140,12 @@ def get_sentiment_word_dict():
         sentiment_dict['Weak Modal'].append(row_dict['Word'])
     for row_dict in sentiment_word_df7.to_dict(orient='records'):
         sentiment_dict['Constraining'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df8.to_dict(orient='records'):
+        sentiment_dict['Superfluous'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df9.to_dict(orient='records'):
+        sentiment_dict['Interesting'].append(row_dict['Word'])
+    for row_dict in sentiment_word_df10.to_dict(orient='records'):
+        sentiment_dict['Modal'].append(row_dict['Word'])
+
     # Return the dictionary of sentiment words    
     return sentiment_dict
