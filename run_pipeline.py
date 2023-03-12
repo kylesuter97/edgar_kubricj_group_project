5144c5b696b5a8a0464c337acbbfa88dc548f286
Arun Jandor <arunjandor@kubrickgroup.com>
2023-03-12 14:41:28 +0000

    RestoringRunPipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -9,10 +9,10 @@ import os
 tickers = edgar_data.get_sp100()
 def main(list_of_tickers, input_folder, output_folder, output_file, start_date, end_date):
     repo_wd = os.getcwd() #Recordws local repository
-    # for i in list_of_tickers:
-    #     edgar_downloader.download_files_10k(i, input_folder) #Downloads all html files to folder
+    for i in list_of_tickers:
+        edgar_downloader.download_files_10k(i, input_folder) #Downloads all html files to folder
 
-    # edgar_cleaner.write_clean_html_text_files(input_folder, output_folder)#Cleans all files in folder
+    edgar_cleaner.write_clean_html_text_files(input_folder, output_folder)#Cleans all files in folder
     os.chdir(repo_wd) #Returns to local directory
     sentiment_df = edgar_sentiment.write_documents_sentiments(output_folder, output_file)
     financials_df = edgar_data.get_yahoo_data(start_date,end_date, list_of_tickers)
